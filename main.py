d520a201c64081e01cfe717d50e2786ada6aac2f
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-06 20:50:12 +0200

    String - czy zmienna jest liczbą,cyfrą, podkreślenia między przerwami wyrazów,

---
+++ main.py
@@ -0,0 +1,18 @@
+# Day 11 - String4 cdn
+
+nazwaBloga = "Zero To Junior"
+liczba = "12345678"
+
+print(nazwaBloga.swapcase())
+print(nazwaBloga.replace(" ", "_"))
+print(nazwaBloga.isdigit())
+print(nazwaBloga.isalpha())
+print(liczba.isdigit())
+print(liczba.isalpha())
+# Zadanie
+print(nazwaBloga.isdigit())
+print(liczba.isalpha())
+print(liczba.swapcase())
+print(liczba.replace("","_"))
+print(nazwaBloga.replace("","_"))
+print(nazwaBloga.isalpha())
